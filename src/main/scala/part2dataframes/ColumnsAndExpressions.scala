2d7bb85d433561c6f16b39a3ea2b16898f9637d0
Nick <nikolayvaklinov@yahoo.com>
2020-11-10 20:44:50 +0000

    creating the spark session

--- src/main/scala/part2dataframes/ColumnsAndExpressions.scala
+++ src/main/scala/part2dataframes/ColumnsAndExpressions.scala
@@ -1,5 +1,14 @@
 package part2dataframes
 
+import org.apache.spark.sql.SparkSession
+
 object ColumnsAndExpressions extends App {
+
+  val spark = SparkSession.builder()
+    .appName("DF Columns and Expressions")
+    .config("spark.master", "local")
+    .getOrCreate()
+  
+
 
 }
