b1b436a463f452c655da1deb5655d720ee04a445
Nick <nikolayvaklinov@yahoo.com>
2020-11-10 21:08:40 +0000

    'select' plus 'col' plus 'column' functions

--- src/main/scala/part2dataframes/ColumnsAndExpressions.scala
+++ src/main/scala/part2dataframes/ColumnsAndExpressions.scala
@@ -1,6 +1,7 @@
 package part2dataframes
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, column}
 
 object ColumnsAndExpressions extends App {
 
@@ -8,7 +9,23 @@ object ColumnsAndExpressions extends App {
     .appName("DF Columns and Expressions")
     .config("spark.master", "local")
     .getOrCreate()
-  
+
+  val carsDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/cars.json")
+
+  carsDF.show()
+
+  // Columns
+  val firstColumn = carsDF.col("Name")
+
+  // selecting a.k.a. projecting
+  val carNamesDF = carsDF.select(firstColumn)
+
+  carsDF.select(
+    col("Name"),
+    column("Acceleration") // col and column are exactly the same
+  )
 
 
 }
