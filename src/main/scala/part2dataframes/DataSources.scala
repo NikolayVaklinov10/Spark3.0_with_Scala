1437d233eee632a28cde2a3ef57388abaccffa6f
Nick <nikolayvaklinov@yahoo.com>
2020-11-10 18:42:26 +0000

    reading and writing sources

--- src/main/scala/part2dataframes/DataSources.scala
+++ src/main/scala/part2dataframes/DataSources.scala
@@ -1,6 +1,8 @@
 package part2dataframes
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType, StructField, StructType}
+import part2dataframes.DataFramesBasics.{carsDFSchema, spark}
 
 object DataSources extends App {
 
@@ -9,6 +11,58 @@ object DataSources extends App {
     .appName("Data Sources and Formats")
     .config("spark.master", "local")
     .getOrCreate()
-  
+
+  val carsSchema = StructType(Array(
+    StructField("Name", StringType),
+    StructField("Miles_per_Gallon", IntegerType),
+    StructField("Cylinders", IntegerType),
+    StructField("Displacement", IntegerType),
+    StructField("Horsepower", IntegerType),
+    StructField("Weight_in_lbs", IntegerType),
+    StructField("Acceleration", DoubleType),
+    StructField("Year", StringType),
+    StructField("Origin", StringType)
+  ))
+
+  /*
+    Reading a DF:
+    - format
+    - schema or inferSchema = true
+    - path
+    - zero or more options
+   */
+
+  val carsDF = spark.read
+    .format("json")
+    .schema(carsSchema) // enforce a schema
+    .option("mode", "failFast") // dropMalformed, permissive (default)
+    .option("path", "src/main/resources/data/cars_dates.json")
+    .load()
+
+//  carsDF.show()
+
+  // alternative reading with options map
+  val carsDFWithOptionsMap = spark.read
+    .format("json")
+    .options(Map(
+      "mode" -> "failFast",
+      "path" -> "src/main/resources/data/cars_dates.json",
+      "inferSchema" -> "true"
+    ))
+    .load()
+
+  /*
+   Writing DFs
+   - format
+   - save mode = overwrite, append, ignore, errorIfExists
+   - path
+   - zero or more options
+   */
+  carsDF.write
+    .format("json")
+    .mode(SaveMode.Overwrite)
+    .save("src/main/resources/data/cars_dupe.json")
+
+
 
 }
