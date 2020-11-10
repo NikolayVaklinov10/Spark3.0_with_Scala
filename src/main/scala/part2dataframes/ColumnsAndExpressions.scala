1af9fd0327675fac84b0cd0b47f6836de9c1056c
Nick <nikolayvaklinov@yahoo.com>
2020-11-10 22:21:11 +0000

    expr and some cool filter functions

--- src/main/scala/part2dataframes/ColumnsAndExpressions.scala
+++ src/main/scala/part2dataframes/ColumnsAndExpressions.scala
@@ -1,6 +1,7 @@
 package part2dataframes
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.expr
 import org.apache.spark.sql.functions.{col, column}
 
 object ColumnsAndExpressions extends App {
@@ -22,10 +23,69 @@ object ColumnsAndExpressions extends App {
   // selecting a.k.a. projecting
   val carNamesDF = carsDF.select(firstColumn)
 
+  // various select methods
+  import spark.implicits._
+
   carsDF.select(
     col("Name"),
-    column("Acceleration") // col and column are exactly the same
+    column("Acceleration"), // col and column are exactly the same
+    column("Weight_in_lbs"),
+    'Year, // Scala Symbol, auto-converted to column
+    $"Horsepower", //fancier interpolated string, returns a Column object
+    expr("Origin") // EXPRESSION
   )
+
+  // select with plain column names
+  carsDF.select("Name", "Year")
+
+  //EXPRESSIONS
+  val simplestExpression = carsDF.col("Weight_in_lbs")
+  val weightInKgExpression = carsDF.col("Weight_in_lbs") / 2.2
+
+  val carsWithWeightsDF = carsDF.select(
+    col("Name"),
+    col("Weight_in_lbs"),
+    weightInKgExpression.as("Weight_in_kg"),
+    expr("Weight_in_lbs / 2.2").as("Weight_in_kg_2")
+  )
+
+  // selectExpr
+  val carsWithSelectExprWeightsDF = carsDF.selectExpr(
+    "Name",
+    "Weight_in_lbs",
+    "Weight_in_lbs / 2.2"
+  )
+
+  // DF processing
+
+  // adding a column
+  val carsWithKg3DF = carsDF.withColumn("Weight_in_kg_3", col("Weight_in_lbs") / 2.2)
+
+  // renaming a column
+  val carsWithColumnRenamed = carsDF.withColumnRenamed("Weight_in_lbs", "Weight_in_pounds")
+
+  // careful with column names
+  carsWithColumnRenamed.selectExpr("`Weight in pounds`")
+
+  // remove a column
+  carsWithColumnRenamed.drop("Cylinders", "Displacement")
+
+  // filtering
+  val europeanCarsDF = carsDF.filter(col("Origin") =!= "USA")
+  val europeanCarsDF2 = carsDF.where(col("Origin") =!= "USA")
+  // filtering with expression strings
+  val americanCarsDF = carsDF.filter("Origin = 'USA'")
+  val americanPowerfulCarsDF = carsDF.filter(col("Origin") === "USA").filter(col("Horsepower") > 150)
+  val americanPowerfulCarsDF2 = carsDF.filter(col("Origin") === "USA" and col("Horsepower") > 150)
+  val americanPowerfulCarsDF3 = carsDF.filter("Origin = 'USA' and Horsepower > 150")
+
+  // unioning = adding more rows
+  val moreCarsDF = spark.read.option("inferSchema", "true").json("src/main/resources/data/more_cars.json")
+  val allCarsDF = carsDF.union(moreCarsDF) // works if the DFs have the same schema
+
+  // distinct values
+  val allCountriesDF = carsDF.select("Origin").distinct()
+
 
 
 }
