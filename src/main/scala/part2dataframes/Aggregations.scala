565e30b2f05ea5f4a87c4ff00e89cecedf6ec553
Nick <nikolayvaklinov@yahoo.com>
2020-11-11 11:39:04 +0000

    initiating spark session

---
+++ src/main/scala/part2dataframes/Aggregations.scala
@@ -0,0 +1,12 @@
+package part2dataframes
+
+import org.apache.spark.sql.SparkSession
+
+object Aggregations extends App {
+
+  val spark = SparkSession.builder()
+    .appName("Aggregations and Grouping")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+}
