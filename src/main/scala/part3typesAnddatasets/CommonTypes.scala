b7f0db206a51fe42bfbbea0ce90105c14baf00a8
Nick <nikolayvaklinov@yahoo.com>
2020-11-11 16:51:08 +0000

    adding values to the data plus chaining  some computation and so on

--- src/main/scala/part3typesAnddatasets/CommonTypes.scala
+++ src/main/scala/part3typesAnddatasets/CommonTypes.scala
@@ -1,6 +1,7 @@
 package part3typesAnddatasets
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, lit}
 
 object CommonTypes extends App {
 
@@ -8,6 +9,23 @@ object CommonTypes extends App {
     .appName("Common Types and Sets")
     .config("spark.master", "local")
     .getOrCreate()
+
+  val moviesDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+  // adding a plain value to a DF
+  moviesDF.select(col("Title"), lit(47).as("plain_value"))
+
+  // Booleans
+  val dramaFilter = col("Major_Genre") equalTo "Drama"
+  val goodRatingFilter = col("IMDB_Rating") > 7.0
+  val preferredFilter = dramaFilter and goodRatingFilter
+
+  moviesDF.select("Title").where(dramaFilter)
+  // + multiple ways of filtering
+
+  val moviesWithGoodnessFlagsDF = moviesDF.select(col("Title"), preferredFilter.as("good_movie")).show()
 
 
 
