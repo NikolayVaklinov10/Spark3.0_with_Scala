d4f15f9a069c21a99f298c3bd9b2bc92bf5ee4be
Nick <nikolayvaklinov@yahoo.com>
2020-11-11 12:12:17 +0000

    the following functions: count, countDistinct, approx_count_distinct, min, sum etc.

--- src/main/scala/part2dataframes/Aggregations.scala
+++ src/main/scala/part2dataframes/Aggregations.scala
@@ -1,6 +1,7 @@
 package part2dataframes
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{approx_count_distinct, col, count, countDistinct, min, sum}
 
 object Aggregations extends App {
 
@@ -8,5 +9,42 @@ object Aggregations extends App {
     .appName("Aggregations and Grouping")
     .config("spark.master", "local")
     .getOrCreate()
+
+
+  val moviesDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+  // counting
+  val genresCountDF = moviesDF.select(count(col("Major_Genre"))) // all the values except null
+  // alternative method which does the same thing
+  moviesDF.selectExpr("count(Major_Genre)")
+
+  // counting null
+  moviesDF.select(count("*")).show() // count all the rows, and will INCLUDE nulls
+
+  // counting distinct
+  moviesDF.select(countDistinct(col("Major_Genre"))).show()
+
+  // approximate count
+  moviesDF.select(approx_count_distinct(col("Major_Genre")))
+
+  // min and max
+  val minRatingDF = moviesDF.select(min(col("IMDB_Rating")))
+
+  // min and max alternative with selectExpr
+  moviesDF.selectExpr("min(IMDB_Rating)")
+
+  // sum
+  moviesDF.select(sum(col("US_Gross")))
+  // or
+  moviesDF.selectExpr("sum(US_Gross)")
+
+
+//  genresCountDF.show()
+
+
+
+
 
 }
