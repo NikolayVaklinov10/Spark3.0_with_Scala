2f259ee9158d7b85a23e4a9ae1af1b6e4e5e0df3
Nick <nikolayvaklinov@yahoo.com>
2020-11-11 21:44:37 +0000

    dates code

--- src/main/scala/part3typesAnddatasets/ComplexTypes.scala
+++ src/main/scala/part3typesAnddatasets/ComplexTypes.scala
@@ -1,6 +1,7 @@
 package part3typesAnddatasets
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, current_date, current_timestamp, datediff, to_date}
 
 object ComplexTypes extends App {
 
@@ -9,5 +10,16 @@ object ComplexTypes extends App {
     .config("spark.master", "local")
     .getOrCreate()
 
+
+  val moviesDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/movies.json")
+
+  // Dates
+  moviesDF.select(col("Title"), to_date(col("Release_Date"), "dd-MMM-yy").as("Actual_Release"))
+    .withColumn("Today", current_date())
+    .withColumn("Right_Now", current_timestamp())
+    .withColumn("Movie_Age", datediff(col("Today"), col("Actual_Release")) / 365)
+    .show()
 
 }
