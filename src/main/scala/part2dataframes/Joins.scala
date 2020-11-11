f3a1e78401b9b70db5b62c8defe112a4e58ed383
Nick <nikolayvaklinov@yahoo.com>
2020-11-11 14:03:53 +0000

    the boilerplate

--- src/main/scala/part2dataframes/Joins.scala
+++ src/main/scala/part2dataframes/Joins.scala
@@ -1,6 +1,14 @@
 package part2dataframes
 
+import org.apache.spark.sql.SparkSession
+
 object Joins extends App {
+
+  val spark = SparkSession.builder()
+    .appName("Joins")
+    .config("spark.master", "local")
+    .getOrCreate()
+
   
 
 }
