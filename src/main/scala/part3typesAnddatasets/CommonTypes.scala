76e3327ad4c972d6604437f826afa850b7eae930
Nick <nikolayvaklinov@yahoo.com>
2020-11-11 17:24:18 +0000

    some data science related function stat.corr() is an action

--- src/main/scala/part3typesAnddatasets/CommonTypes.scala
+++ src/main/scala/part3typesAnddatasets/CommonTypes.scala
@@ -1,7 +1,7 @@
 package part3typesAnddatasets
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, lit}
+import org.apache.spark.sql.functions.{col, lit, not}
 
 object CommonTypes extends App {
 
@@ -25,8 +25,20 @@ object CommonTypes extends App {
   moviesDF.select("Title").where(dramaFilter)
   // + multiple ways of filtering
 
-  val moviesWithGoodnessFlagsDF = moviesDF.select(col("Title"), preferredFilter.as("good_movie")).show()
+  val moviesWithGoodnessFlagsDF = moviesDF.select(col("Title"), preferredFilter.as("good_movie"))
 
+  // filter on a boolean column
+  moviesWithGoodnessFlagsDF.where("good_movie") // where(col("good_movie") === "true")
+
+  // negations
+  moviesWithGoodnessFlagsDF.where(not(col("good_movie")))
+
+  // Numbers
+  // math operators
+  val moviesAvgRatingsDF = moviesDF.select(col("Title"), (col("Rotten_Tomatoes_Rating") / 10 + col("IMDB_Rating")) / 2)
+
+  // correlation = number between -1 and 1
+  println(moviesDF.stat.corr("Rotten_Tomatoes_Rating", "IMDB_Rating") /* corr is an ACTION */)
 
 
 }
