9e7f9963036efc1b0880f7696af64c9f099d4497
Nick <nikolayvaklinov@yahoo.com>
2020-11-11 14:21:19 +0000

    the inner join bit

--- src/main/scala/part2dataframes/Joins.scala
+++ src/main/scala/part2dataframes/Joins.scala
@@ -9,6 +9,22 @@ object Joins extends App {
     .config("spark.master", "local")
     .getOrCreate()
 
-  
+  val guitarsDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/guitars.json")
+
+  val guitaristsDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/guitarPlayers.json")
+
+  val bandsDF = spark.read
+    .option("inferSchema", "true")
+    .json("src/main/resources/data/bands.json")
+
+  // inner joins
+  val joinCondition = guitaristsDF.col("band") === bandsDF.col("id")
+  val guitaristsBandsDF = guitaristsDF.join(bandsDF, joinCondition,"inner")
+  guitaristsBandsDF.show
+
 
 }
