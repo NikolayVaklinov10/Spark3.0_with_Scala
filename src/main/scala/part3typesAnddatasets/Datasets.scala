686e0144028be22b859c51ee111eb4b1c71ff714
Nick <nikolayvaklinov@yahoo.com>
2020-11-12 11:13:21 +0000

    the steps to convert data frame to dataset

--- src/main/scala/part3typesAnddatasets/Datasets.scala
+++ src/main/scala/part3typesAnddatasets/Datasets.scala
@@ -1,5 +1,7 @@
 package part3typesAnddatasets
 
+import java.sql.Date
+
 import org.apache.spark.sql.{Dataset, Encoders, SparkSession}
 
 object Datasets extends App {
@@ -20,6 +22,38 @@ object Datasets extends App {
   // convert a DF to a Dataset
   implicit val intEncoder = Encoders.scalaInt
   val numbersDS: Dataset[Int] = numbersDF.as[Int]
+
+  // dataset of a complex type
+  // 1 - define your case class
+  case class Car(
+                  Name: String,
+                  Miles_per_Gallon: Option[Double],
+                  Cylinders: Long,
+                  Displacement: Double,
+                  Horsepower: Option[Long],
+                  Weight_in_lbs: Long,
+                  Acceleration: Double,
+                  Year: Date,
+                  Origin: String
+                )
+
+  // 2 - read the DF from the file
+  def readDF(filename: String) = spark.read
+    .option("inferSchema", "true")
+    .json(s"src/main/resources/data/$filename")
+
+  val carsDF = readDF("cars.json")
+
+  // 3 - define an encoder (importing the implicits)
+  import spark.implicits._
+  // 4 - convert the DF to DS
+  val carsDS = carsDF.as[Car]
+
+  // DS collection functions
+  numbersDS.filter(_ < 100)
+
+  // map, flatMap, fold, reduce, for comprehensions ...
+  val carNamesDS = carsDS.map(car => car.Name.toUpperCase())
 
 
 }
