712586b9e18bedceab5dbee1b53cd1e16b48430f
Nick <nikolayvaklinov@yahoo.com>
2020-11-12 09:58:34 +0000

    the usual spark session code plus the formatting and accessing the file, on top of that converting data frame to dataset with Encoders.scalaInt

---
+++ src/main/scala/part3typesAnddatasets/Datasets.scala
@@ -0,0 +1,25 @@
+package part3typesAnddatasets
+
+import org.apache.spark.sql.{Dataset, Encoders, SparkSession}
+
+object Datasets extends App {
+
+  val spark = SparkSession.builder()
+    .appName("Datasets")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val numbersDF = spark.read
+    .format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load("src/main/resources/data/numbers.csv")
+
+  numbersDF.printSchema()
+
+  // convert a DF to a Dataset
+  implicit val intEncoder = Encoders.scalaInt
+  val numbersDS: Dataset[Int] = numbersDF.as[Int]
+
+
+}
